4a13289b453d8364caa47a92384c15cfad1f7f53
town <dash>
2026-08-25 11:31:00 -0400

    teams link

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,4 +2,4 @@ export function cn(...classes: Array<string | false | null | undefined>) {
   return classes.filter(Boolean).join(" ");
 }
 
-export const meetingUrl = process.env.NEXT_PUBLIC_MEETING_URL || "https://teams.microsoft.com/l/meetup-join/replace-with-your-link";
+export const meetingUrl = process.env.NEXT_PUBLIC_MEETING_URL || "https://teams.live.com/l/invite/FEAUzJt7znCvo99Zw?v=g1";
